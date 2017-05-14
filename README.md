3dc3113743cca4edd741a6f41adef3d765a8c1ea
bayotop <martin.bajanik@gmail.com>
2017-05-14 11:28:54 +0200

    parametrize reports path

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Resource Difference Checker
 
+The idea is to track updates made to resource avaialable on the web. Preferred are JS files or whole HTML pages.
+The are two important python scripts ```initialize.py``` and ```diffcheck.py```. The latter mentioned is ment to be scheduled to run once a day.
+
+Before a difference report is created jsbeautifier (https://github.com/beautify-web/js-beautify) is run on the compared contents.  
+
 # Setup
 
 ```
-$ pip install jsbeautifier
+$ python3.6 -m pip install jsbeautifier
+$ git clone https://github.com/bayotop/resdiffcheck
+$ cd resdiffcheck/resdiffcheck
+$ echo "http://example.com/js/script.js" >> resources.txt
+$ python3.6 initialize.py resources.txt data/resources.db
+$ python3.6 diffcheck.py data/resources.db ~/public_html/resdiffcheck/reports
+
 ```
 
 
--- resdiffcheck/diffcheck.py
+++ resdiffcheck/diffcheck.py
@@ -14,9 +14,9 @@ from classes.dbmanager import ResourceStorage
 from classes.reporter import HtmlReport
 import helpers
 
-def get_report_path():
+def get_reports_path(path=None):
     today = date.today()
-    return "reports/%s/%s/" % (today.month, today.day)
+    return "{0}/{1}/{2}/".format(path, today.month, today.day) if path else "reports/{0}/{1}/".format(today.month, today.day)
 
 def check_differences(resources, report):
     changed_resources = []
@@ -34,18 +34,20 @@ def check_differences(resources, report):
     return changed_resources
 
 if __name__ == "__main__":
-    if len(sys.argv) != 1 and len(sys.argv) != 2:
-        print("Usage: %s [db_path]" % sys.argv[0])
+    if len(sys.argv) != 1 and len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("Usage: %s [db_path] [reports_path]" % sys.argv[0])
         sys.exit()
 
     logging.basicConfig(filename='process.log',level=logging.DEBUG)
 
-    db = sys.argv[1] if len(sys.argv) == 2 else "data/resources.db"
+    db = sys.argv[1] if len(sys.argv) >= 2 else "data/resources.db"
+    reports_path = get_reports_path(sys.argv[2]) if len(sys.argv) == 3 else get_reports_path()
+
     storage = ResourceStorage(db)
     if not storage.load():
         sys.exit()
 
-    report = HtmlReport(get_report_path(), "diff.html")
+    report = HtmlReport(reports_path, "diff.html")
 
     changed_resources = check_differences(storage.getall(), report)
 
